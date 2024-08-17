3b8bbf9da548b1cd1b27e2f5d001b9aa31a03b4e
alphainfinitus <kumar.mridul000@gmail.com>
2024-08-18 03:42:51 +0530

    chore: :fire: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:potential_plus/constants/text_literals.dart';
+import 'package:potential_plus/firebase_options.dart';
 import 'package:potential_plus/providers/current_theme_provider.dart';
 import 'package:potential_plus/screens/home/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 	runApp(const ProviderScope(child: AppRootWidget()));
 }
 
